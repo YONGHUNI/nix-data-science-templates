6e57919f9ced4c7fc53e3e8e98549020bad5ec21
Yong-hun Suh <62800337+YONGHUNI@users.noreply.github.com>
2026-08-21 00:43:38 -0400

    feat: add R renv template

---
+++ r-renv/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "R data science environment with renv";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          R
+          rPackages.renv
+          pkg-config
+          gcc
+          gfortran
+        ];
+      };
+    };
+}
