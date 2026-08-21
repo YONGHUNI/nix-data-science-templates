94177c56d4eead2eede076d1621fcfde542e6a0e
Yong-hun Suh <62800337+YONGHUNI@users.noreply.github.com>
2026-08-21 00:43:03 -0400

    feat: add Python Conda flake

---
+++ python-conda/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Python data science environment with micromamba";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          micromamba
+          conda-lock
+        ];
+
+        shellHook = ''
+          alias mamba=micromamba
+        '';
+      };
+    };
+}
