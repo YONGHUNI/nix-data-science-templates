4d3885e5dd06a033d03fbc4b75aad5e6107b1225
Yong-hun Suh <62800337+YONGHUNI@users.noreply.github.com>
2026-08-21 01:25:25 -0400

    fix(python-conda): enable bash hashing for micromamba activation

--- python-conda/flake.nix
+++ python-conda/flake.nix
@@ -27,6 +27,7 @@
         ];
 
         shellHook = ''
+          set -h
           export MAMBA_ROOT_PREFIX="$HOME/.mamba"
           eval "$(micromamba shell hook --shell bash)"
           alias mamba=micromamba
